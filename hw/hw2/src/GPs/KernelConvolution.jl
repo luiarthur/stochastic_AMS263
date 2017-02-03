ca8ce41f5db05666e65419043935c02361070bf2
Arthur Lui <luiarthur@gmail.com>
2017-02-02 16:57:42 -0800

    yes

--- hw/hw2/src/GPs/KernelConvolution.jl
+++ hw/hw2/src/GPs/KernelConvolution.jl
@@ -5,7 +5,7 @@ using Distributions, Distances
 export fit
 include("../../../MCMC/MCMC.jl")
 
-gaussian_kernel(d::Float64) = pdf(Normal(), d)
+gaussian_kernel(d::Float64) = pdf(Normal(0,.1), d)
 
 function fit(y::Vector{Float64}, X::Matrix{Float64}, u::Matrix{Float64},
              cs::Vector{Float64}, # [cs_σ², cs_τ²]
@@ -33,13 +33,13 @@ function fit(y::Vector{Float64}, X::Matrix{Float64}, u::Matrix{Float64},
     function ll(log_v::Vector{Float64})
       const sig2 = exp(log_v[1])
       const tau2 = exp(log_v[2])
-      const K = Iₘ/tau2 + MTM/sig2
+      #const K = Iₘ/tau2 + MTM/sig2
 
-      const ld_Cov = -.5 * ( n*log_v[1] + m*log_v[2] + logdet(K) )
-      const log_exp = -.5 * yT*(Iₙ/sig2 - M/sig2 * inv(K) * MT/sig2)*y
+      #const ld_Cov = -.5 * ( n*log_v[1] + m*log_v[2] + logdet(K) )
+      #const log_exp = -.5 * yT*(Iₙ/sig2 - M/sig2 * inv(K) * MT/sig2)*y
 
-      return ld_Cov + log_exp[1]
-      #return logpdf(MvNormal(MMT*tau2 + sig2*Iₙ), y)
+      #return ld_Cov + log_exp[1]
+      return logpdf(MvNormal(MMT*tau2 + sig2*Iₙ), y)
     end
 
     function lp(log_v::Vector{Float64})
@@ -57,17 +57,29 @@ function fit(y::Vector{Float64}, X::Matrix{Float64}, u::Matrix{Float64},
   return out
 end # END OF FIT FUNCTION
 
-#function predict(post::Vector{Vector{Float64}},
-#                 y::Vector{Float64}, X::Matrix{Float64}, u::Matrix{Float64};
-#                 kernel=gaussian_kernel,
-#                 dist=euclidean)
-#
-#  const (n,K) = size(X)
-#  const Iₙ = eye(n)
-#  const m = size(u,1)
-#  const M = [ kernel(euclidean(X[i,:],u[j,:])) for i in 1:n, j in 1:m ]
-#  const MMT = M * M'
-#  return map(p -> rand(MvNormal(MMT*p[2] + p[1]*Iₙ)), post)
-#end
+"""
+Goal: Sample y*|z ~ N(Kz, σ²Iₙ), where K is a kernel and z ~ GP(0, τ²Iₘ)
+This means, y* ~ N(0, σ²Iₙ + KK'τ²)
+"""
+# FIXME: NOT WORKING!!!
+function predict(post::Vector{Vector{Float64}},
+                 y::Vector{Float64}, X::Matrix{Float64}, u::Matrix{Float64};
+                 kernel=gaussian_kernel,
+                 dist=euclidean)
+
+  const n = size(X,1)
+  const m = size(u,1)
+  const Iₙ = eye(n)
+  const Iₘ = eye(m)
+
+  const K = [ kernel(dist(X[i,:],u[j,:])) for i in 1:n, j in 1:m ]
+  const KKT = K*K'
+
+  function pred(sig2::Float64, tau2::Float64)
+    return rand( MvNormal(sig2*Iₙ + KKT*tau2) )
+  end
+
+  return map(p -> pred(p[1],p[2]), post)
+end
 
 end # END OF MODULE
