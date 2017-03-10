4af2cb4b69c34abb27b10ecfd9ad3d27d61e8e8c
Arthur Lui <luiarthur@gmail.com>
2017-03-09 17:05:33 -0800

    yes

--- hw/hw5/src/hmm.R
+++ hw/hw5/src/hmm.R
@@ -11,19 +11,54 @@ gen.default.prior <- function(K) {
 # y is data
 # K is number of states
 # B is number of MCMC samples
-hmm <- function(y, K, B=2000, prior=gen.default.prior(K)) {
+hmm <- function(y, K=2, B=2000, prior=gen.default.prior(K)) {
 
   N <- length(y)
-  params <- as.list(1:B)
-  params[[1]]$c <- prior$c
-  params[[1]]$d <- prior$d
 
   update.lam <- function(param) {
+    out <- sapply(1:K, function(j) {
+      idx <- which(param$z==j)
+      c(prior$c[j]+sum(y[idx]),
+        prior$d[j]+length(idx))
+    })
+
+    list(c=out[1,], d=out[2,])
   }
 
   update.Q <- function(param) {
+    z <- param$z
+    Q <- matrix(NA,K,K)
+
+    for (i in 1:K) {
+      for (j in 1:K) {
+        Q[i,j] <- prior$a[i,j]
+        for (n in 2:N) {
+          if (z[n-1]==i && z[n]==j) Q[i,j] <- Q[i,j] + 1
+        }
+      }
+    }
+
+    Q
   }
 
   update.z <- function(param) {
+    param$z
   }
+
+  init <- NULL
+  init$z <- sample(1:K,N,replace=TRUE)
+  init$lam <- update.lam(init)
+  init$Q <- update.Q(init)
+
+  params <- as.list(1:B)
+  params[[1]] <- init
+  for (i in 2:B) {
+    curr <- params[[i-1]]
+    curr$lam <- update.lam(curr)
+    curr$Q <- update.Q(curr)
+    curr$z <- update.z(curr)
+    params[[i]] <- curr
+  }
+
+  return(params)
 }
--- hw/hw5/src/main.R
+++ hw/hw5/src/main.R
@@ -1,2 +1,5 @@
-mvt <- read.table("../dat/lamb.dat",header=TRUE)$movements
-plot(mvt,type='l')
+source('hmm.R')
+y <- read.table("../dat/lamb.dat",header=TRUE)$movements
+plot(y,type='l',main='Lamb Movements')
+
+out <- hmm(y,B=10)
