2b19808abb5aa37a295367d9887dd67553515174
Arthur Lui <luiarthur@gmail.com>
2017-03-22 18:57:12 -0700

    yes

--- hw/project/src/IBP/test.jl
+++ hw/project/src/IBP/test.jl
@@ -1,4 +1,5 @@
 using RCall
+using Distributions
 srand(1);
 
 include("LFM.jl")
@@ -18,7 +19,7 @@ K = 10
 N = 100
 sig2_true = .04
 (K_true, D) = size(A)
-Z_true = rand(0:1, N, K_true)
+Z_true = rand(Multinomial(2, [.25,.25,.25,.25]),N)'
 X = Z_true * A + randn(N,D)*sqrt(sig2_true)
 IBP.plot(Z_true)
 
@@ -37,7 +38,7 @@ cs_v = cov(hcat(unique(map(o->o.v, tmp))...)') + eye(K)*.1
 
 @time out = LFM.fit(X,K=K,B=500,burn=1000,printFreq=1,
                     sig2_true=sig2_true,
-                    init=tmp[end],cs_v=cs_v)
+                    init=tmp[end],cs_v=cs_v);
 
 sig2_out = map(o -> o.sig2, out)
 R"plotPost($sig2_out)";
@@ -62,6 +63,24 @@ IBP.plot(Z_mean-Z_true_ext)
 #IBP.plot(A_mean)
 #IBP.plot(A)
 
+function estA(Z::Matrix{Int64})
+  const positiveCol = find(sum(Z,1) .> 0)
+  const Z1 = Z[:,positiveCol]
+  inv(Z1'Z1)*Z1'*X
+end
+
+A_est = map(estA, Z_out)
+A_mean = reduce(+, A_est) / length(A_est)
+
+IBP.plot(reshape(A_mean[1,:],6,6))
+IBP.plot(reshape(A_mean[2,:],6,6))
+IBP.plot(reshape(A_mean[3,:],6,6))
+IBP.plot(reshape(A_mean[4,:],6,6))
+IBP.plot(reshape(A_mean[5,:],6,6))
+IBP.plot(reshape(A_mean[6,:],6,6))
+IBP.plot(reshape(A_mean[7,:],6,6))
+IBP.plot(reshape(A_mean[8,:],6,6))
+IBP.plot(reshape(A_mean[9,:],6,6))
 
 #=
 include("test.jl")
